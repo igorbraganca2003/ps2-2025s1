f08c0d77ef2b3ab7cdafbadf013aaabdb65a2c2a
Igor Bragança <igor.braganca2003@gmail.com>
2025-02-20 00:30:30 +0000

    Concertando Lab02

--- Lab02/src/Main.java
+++ Lab02/src/Main.java
@@ -4,8 +4,8 @@ class Main {
     public static void main(String[] args) {
        Conta[] contas = { 
         new ContaInfinita(1111, 50),
-        new contaBancaria(2222, 99),
-        new contaBancaria(3333, 1000),
+        new ContaBancaria(2222, 99),
+        new ContaBancaria(3333, 1000),
         new ContaEspecial(4444, 800, 200)};
        for (int i=0; i<contas.length; i++){
         contas[i].sacar(900);
